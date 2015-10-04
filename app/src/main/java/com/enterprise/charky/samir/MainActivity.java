f52a473f352ab37a35ecd04814201bbb40de5672
charky <gfcharky@gmx.de>
2015-10-04 15:28:55 +0200

    Generic Generation of custom IR-Code-Buttons. Sliding transition between basic and advanced view. Runnable but not fully tested.

--- app/src/main/java/com/enterprise/charky/samir/MainActivity.java
+++ app/src/main/java/com/enterprise/charky/samir/MainActivity.java
@@ -3,17 +3,35 @@ package com.enterprise.charky.samir;
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.transition.Scene;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 
+import com.enterprise.charky.samir.irclibrary.SamsungIRCodes;
 import com.enterprise.charky.samir.irtransmitter.GenericIRCodes;
 import com.enterprise.charky.samir.irtransmitter.IRTransmitter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     public IRTransmitter irTransmitter;
     private static final String TAG = "SamIR_MainActivity";
+    private static final int SCENE_STATE_BASIC = 100;
+    private static final int SCENE_STATE_ADVANCED = 200;
+
+    private Scene basicsScene;
+    private Scene advancedScene;
+    private Transition slidingTransition;
+    private int sceneState;
+    private GridLayout advancedGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +39,44 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         //Create IRTransmitter
-        //irTransmitter = new IRTransmitter(this, new SamsungIRCodes());
+        irTransmitter = new IRTransmitter(this, new SamsungIRCodes());
 
         setContentView(R.layout.activity_main);
+
+        initScene();
+
+        initAdvancedButtons();
+
+    }
+
+    private void initScene(){
+        // Create the scene root for the scenes in this app
+        ViewGroup frameMain = (ViewGroup) findViewById(R.id.frame_main);
+        View vi  = getLayoutInflater().inflate(R.layout.advanced_remotes, null);
+
+        advancedGrid = (GridLayout) vi.findViewById(R.id.advanced_grid);
+
+        // Create the scenes
+        basicsScene = Scene.getSceneForLayout(frameMain, R.layout.basic_remotes, this);
+        advancedScene = new Scene(frameMain,vi);
+
+        // Create scene transition
+        slidingTransition = new Slide();
+        sceneState = SCENE_STATE_BASIC;
+
+    }
+
+    private void initAdvancedButtons() {
+
+        advancedGrid.removeAllViews();
+
+        for(String customButtonName: irTransmitter.getGenericIRCodes().getCustomCommandNames()){
+            Button bt = new Button(this);
+            bt.setText(customButtonName);
+            bt.setOnClickListener(this);
+            advancedGrid.addView(bt,dpToPixels(192),dpToPixels(64));
+        }
+
     }
 
     public void RemoteButtonClick(View v) {
@@ -130,7 +183,35 @@ public class MainActivity extends Activity {
             return true;
         }
 
+        if (id == R.id.action_advanced) {
+            if(sceneState == SCENE_STATE_BASIC) {
+                sceneState = SCENE_STATE_ADVANCED;
+                item.setTitle(getString(R.string.action_basic));
+                TransitionManager.go(advancedScene, slidingTransition);
+            }else{
+                sceneState = SCENE_STATE_BASIC;
+                item.setTitle(getString(R.string.action_advanced));
+                TransitionManager.go(basicsScene, slidingTransition);
+            }
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View v) {
+        String btText = ((Button)v).getText().toString();
+        GenericIRCodes genericIRCodes = irTransmitter.getGenericIRCodes();
+        try {
+            irTransmitter.sendIR(genericIRCodes.getCustomCommand(btText));
+        } catch (IRTransmitter.NoIREmitterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int dpToPixels(int dps){
+        final float scale = getResources().getDisplayMetrics().density;
+        return (int) (dps * scale + 0.5f);
+    }
 }
